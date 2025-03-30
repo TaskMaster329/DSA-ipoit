ff92a552ab24737c4dd7d6b0bc002b7d674316b0
Aliyev <taskm950@gmail.com>
2025-03-30 15:46:58 +0300

    lesson 2 complete

--- src/by/it/group410972/aliyev/lesson02/A_VideoRegistrator.java
+++ src/by/it/group410972/aliyev/lesson02/A_VideoRegistrator.java
@@ -1,13 +1,15 @@
 package by.it.group410972.aliyev.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 /*
-Даны события events
-реализуйте метод calcStartTimes, так, чтобы число включений регистратора на
-заданный период времени (1) было минимальным, а все события events
-были зарегистрированы.
-Алгоритм жадный. Для реализации обдумайте надежный шаг.
+ Даны события events
+ реализуйте метод calcStartTimes, так, чтобы число включений регистратора на
+ заданный период времени (1) было минимальным, а все события events
+ были зарегистрированы.
+ Алгоритм жадный. Для реализации обдумайте надежный шаг.
 */
 
 public class A_VideoRegistrator {
@@ -15,30 +17,25 @@ public class A_VideoRegistrator {
     public static void main(String[] args) {
         A_VideoRegistrator instance = new A_VideoRegistrator();
         double[] events = new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
-        List<Double> starts = instance.calcStartTimes(events, 1); //рассчитаем моменты старта, с длинной сеанса 1
-        System.out.println(starts);                            //покажем моменты старта
+        List<Double> starts = instance.calcStartTimes(events, 1); // рассчитываем моменты старта с длительностью сеанса 1
+        System.out.println(starts); // показываем моменты старта
     }
 
-    //модификаторы доступа опущены для возможности тестирования
     List<Double> calcStartTimes(double[] events, double workDuration) {
-        //events - события которые нужно зарегистрировать
-        //timeWorkDuration время работы видеокамеры после старта
-        List<Double> result;
-        result = new ArrayList<>();
-        int i = 0;                              //i - это индекс события events[i]
-        //Комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
-        //Подготовка к жадному поглощению массива событий
-        //hint: сортировка Arrays.sort обеспечит скорость алгоритма
-        //C*(n log n) + C1*n = O(n log n)
+        List<Double> result = new ArrayList<>();
+        Arrays.sort(events); // сортируем события по времени
+        int i = 0;
 
-        //пока есть незарегистрированные события
-        //получим одно событие по левому краю
-        //и запомним время старта видеокамеры
-        //вычислим момент окончания работы видеокамеры
-        //и теперь пропустим все покрываемые события
-        //за время до конца работы, увеличивая индекс
+        while (i < events.length) {
+            double startTime = events[i]; // фиксируем момент включения регистратора
+            result.add(startTime);
+            double endTime = startTime + workDuration; // рассчитываем момент выключения
 
-
-        return result;                        //вернем итог
+            // пропускаем все события, которые попадают в интервал работы регистратора
+            while (i < events.length && events[i] <= endTime) {
+                i++;
+            }
+        }
+        return result;
     }
 }
--- src/by/it/group410972/aliyev/lesson02/C_GreedyKnapsack.java
+++ src/by/it/group410972/aliyev/lesson02/C_GreedyKnapsack.java
@@ -1,20 +1,7 @@
 package by.it.group410972.aliyev.lesson02;
-/*
-Даны
-1) объем рюкзака 4
-2) число возможных предметов 60
-3) сам набор предметов
-    100 50
-    120 30
-    100 50
-Все это указано в файле (by/it/a_khmelev/lesson02/greedyKnapsack.txt)
-
-Необходимо собрать наиболее дорогой вариант рюкзака для этого объема
-Предметы можно резать на кусочки (т.е. алгоритм будет жадным)
- */
-
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class C_GreedyKnapsack {
@@ -23,33 +10,35 @@ public class C_GreedyKnapsack {
         InputStream inputStream = C_GreedyKnapsack.class.getResourceAsStream("greedyKnapsack.txt");
         double costFinal = new C_GreedyKnapsack().calc(inputStream);
         long finishTime = System.currentTimeMillis();
-        System.out.printf("Общая стоимость %f (время %d)", costFinal, finishTime - startTime);
+        System.out.printf("Общая стоимость %f (время %d)\n", costFinal, finishTime - startTime);
     }
 
     double calc(InputStream inputStream) throws FileNotFoundException {
         Scanner input = new Scanner(inputStream);
-        int n = input.nextInt();      //сколько предметов в файле
-        int W = input.nextInt();      //какой вес у рюкзака
-        Item[] items = new Item[n];   //получим список предметов
-        for (int i = 0; i < n; i++) { //создавая каждый конструктором
+        int n = input.nextInt();      // сколько предметов в файле
+        int W = input.nextInt();      // какой вес у рюкзака
+        Item[] items = new Item[n];   // список предметов
+
+        for (int i = 0; i < n; i++) {
             items[i] = new Item(input.nextInt(), input.nextInt());
         }
-        //покажем предметы
-        for (Item item : items) {
-            System.out.println(item);
-        }
-        System.out.printf("Всего предметов: %d. Рюкзак вмещает %d кг.\n", n, W);
 
-        //тут необходимо реализовать решение задачи
-        //итогом является максимально воможная стоимость вещей в рюкзаке
-        //вещи можно резать на кусочки (непрерывный рюкзак)
+        Arrays.sort(items); // сортируем предметы по убыванию стоимости на единицу веса
+
         double result = 0;
-        //тут реализуйте алгоритм сбора рюкзака
-        //будет особенно хорошо, если с собственной сортировкой
-        //кроме того, можете описать свой компаратор в классе Item
+        int remainingWeight = W;
 
-        //ваше решение.
+        for (Item item : items) {
+            if (remainingWeight == 0) break;
 
+            if (item.weight <= remainingWeight) {
+                result += item.cost;
+                remainingWeight -= item.weight;
+            } else {
+                result += (item.costPerWeight() * remainingWeight);
+                remainingWeight = 0;
+            }
+        }
 
         System.out.printf("Удалось собрать рюкзак на сумму %f\n", result);
         return result;
@@ -64,20 +53,18 @@ public class C_GreedyKnapsack {
             this.weight = weight;
         }
 
-        @Override
-        public String toString() {
-            return "Item{" +
-                   "cost=" + cost +
-                   ", weight=" + weight +
-                   '}';
+        double costPerWeight() {
+            return (double) cost / weight;
         }
 
         @Override
         public int compareTo(Item o) {
-            //тут может быть ваш компаратор
+            return Double.compare(o.costPerWeight(), this.costPerWeight()); // сортировка по убыванию
+        }
 
-
-            return 0;
+        @Override
+        public String toString() {
+            return "Item{" + "cost=" + cost + ", weight=" + weight + '}';
         }
     }
 }
